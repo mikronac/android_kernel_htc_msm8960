a570576492adfd991952124c569b3dfb09778244
Paul Keith <javelinanddart@gmail.com>
2015-05-19 10:45:46 -0500

    Squashed Revert of Samsung Governor
    Revert "Use Samsung as default CPU Governor"
    
    This reverts commit b8715cbafa37c6a83ac60dfd585c0fe0a7b08c10.
    
    Revert "cpufreq_samsung: Forward port to Linux 3.4"
    
    This reverts commit d5e327d34c6cdbf10bb7d71218a00fa1a72bd017.
    
    Revert "Add Samsung CPU Governor"
    
    This reverts commit 8d2b32d1528b6e2a5f3685dbffa737f533696c59.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,9 +388,6 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAMSUNG)
-extern struct cpufreq_governor cpufreq_gov_samsung;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_samsung)
 #endif
 
 
